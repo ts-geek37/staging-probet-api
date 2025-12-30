48dfac0064da326151fcb8b9438501759aef413c
ts-geek75 <labdhi.shah@techstaunch.com>
2025-12-30 17:58:05 +0530

    Added the link

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   await connectDB();
   server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
   });
 };
 
